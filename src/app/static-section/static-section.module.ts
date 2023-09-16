2c722534e9ba8e5f7a370a926b1ca58d5eeadec3
Michal Kaminski <MichalKaminski.KamienKaze@gmail.com>
2023-09-17 01:13:20 +0200

    reference component added

--- src/app/static-section/static-section.module.ts
+++ src/app/static-section/static-section.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StaticSectionComponent } from './static-section/static-section.component';
+import { ReferenceComponent } from './reference/reference.component';
 
 
 
 @NgModule({
     declarations: [
-        StaticSectionComponent
+        StaticSectionComponent,
+        ReferenceComponent
     ],
     exports: [
         StaticSectionComponent
