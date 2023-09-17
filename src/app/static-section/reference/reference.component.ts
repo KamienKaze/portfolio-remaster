051bfeec44b5c82ae6da37888c83bb1ade20fbb1
Michal Kaminski <MichalKaminski.KamienKaze@gmail.com>
2023-09-17 02:18:37 +0200

    finished reference component

--- src/app/static-section/reference/reference.component.ts
+++ src/app/static-section/reference/reference.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-reference',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./reference.component.scss']
 })
 export class ReferenceComponent {
+  @Input('isActive') private isActive: boolean = false;
+  @Input('referenceId') private referenceId: number = 0;
+  @Input('referenceName') private referenceName: string = "not-assigned";
 
+  public getIsActive(): boolean {
+    return this.isActive;
+  }
+
+  public getReferenceId(): number {
+    return this.referenceId;
+  }
+
+  public getReferenceName(): string {
+    return this.referenceName;
+  }
+
+  public changeIsActiveState(): void {
+    this.isActive = !this.isActive;
+  }
 }
